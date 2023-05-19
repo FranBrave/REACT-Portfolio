f6478efa778c7b3755656de5cc627e9240be6647
Fran <valiente.fco@gmail.com>
2023-05-19 20:17:57 +0200

    add logos on projects

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import "./ProjectCard.scss";
 import { NavLink } from "react-router-dom";
+import angular from "../../assets/angular.png";
+import node from "../../assets/node.png";
+import sass from "../../assets/sass.png";
 
 const ProjectCard = (props) => {
   return (
     <div className="project-card">
       <img src={props.imgSrc} alt="delifood" />
+      <div className="logos">
+        <img src={angular} alt="angular" />
+        <img src={node} alt="node" />
+        <img src={sass} alt="sass" />
+      </div>
       <h2 className="project-title">{props.title}</h2>
       <div className="project-details">
         <p>{props.text}</p>
